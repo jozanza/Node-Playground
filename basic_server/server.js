808cf7a583a5bc6f28bcad1cd87981a66c8d034f
Josiah Savary <josiahsavary@gmail.com>
2012-12-06 10:49:16 -0600

    adjusted server.js whitespace again

--- basic_server/server.js
+++ basic_server/server.js
@@ -3,9 +3,9 @@ var http = require('http')
 // Create a server from the http module
 http.createServer(function (request, response) {
   // Set response headers
-	response.writeHead(200, {'Content-Type': 'text/html'});
+  response.writeHead(200, {'Content-Type': 'text/html'});
   // Set response 
-	response.end('<html><body><h1>Hello World!</h1></body></html>');
+  response.end('<html><body><h1>Hello World!</h1></body></html>');
 })
 // Run server on localhost:3000
 .listen(3000, 'localhost');
